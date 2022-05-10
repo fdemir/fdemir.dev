ad6c50268ab6c0d907b038b951a3e8c22d2bc402
Furkan Demir <frndemir@gmail.com>
2022-05-10 21:51:35 +0300

    :sparkles: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Github } from "mdue";
+import Github from "~icons/mdi/github";
 </script>
 
 <template>
@@ -8,23 +8,19 @@ import { Github } from "mdue";
       <div>
         <div class="inline-flex gap-3 pr-6 flex-col">
           <span class="font-semibold">2022</span>
-          <a href="" class="underline">Furkan Demir</a>
+          <a href="https://fdemir.dev" class="underline">Furkan Demir</a>
         </div>
         <div class="inline-flex gap-3 pr-6 flex-col">
           <span class="font-semibold">Social</span>
           <div>
-            <a href="" class="underline">Twitter</a>
+            <a href="https://twitter.com/furkaneulogy" target="_blank" class="underline">Twitter</a>
             <span class="px-1">/</span>
-            <a href="" class="underline">Github</a>
+            <a href="https://github.com/fdemir" target="_blank" class="underline">Github</a>
           </div>
         </div>
       </div>
       <div>
-        <a
-          href="https://github.com/fdemir"
-          target="_blank"
-          class="hover:text-primary transition-all duration-300"
-        >
+        <a href="https://github.com/fdemir" target="_blank" class="hover:text-primary transition-all duration-300">
           <Github class="text-4xl" />
         </a>
       </div>
