d4c841acb545c23618b743df996dfb69b7590cc8
Furkan Demir <frndemir@gmail.com>
2022-05-10 22:00:12 +0300

    :speech_balloon: update hero text

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="md:h-[500px] h-[400px] flex">
     <div class="md:w-9/12 w-10/12 self-center">
-      <h1
-        class="md:text-7xl text-5xl mb-4 font-serif font-semibold tracking-wide"
-      >
-        I make web applications.
-      </h1>
+      <h1 class="md:text-7xl text-5xl mb-4 font-serif font-semibold tracking-wide">I make web applications.</h1>
       <p class="md:text-3xl text-xl leading-[1.4] font-normal">
         I'm a software developer based in
         <span class="text-primary">World!</span>!
         <br />
-        fixme:My focus creating performant, accessible and beautiful web
-        applications.
+        My focus creating performant, accessible and beautiful web applications.
       </p>
     </div>
   </div>
