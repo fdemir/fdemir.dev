ff87b9720743d92081d189fdcd59644e2e3c08de
Furkan Demir <frndemir@gmail.com>
2022-05-14 10:11:26 +0300

    :lipstick: theme picker navbar compability

--- components/Theme/Toggle.vue
+++ components/Theme/Toggle.vue
@@ -1,10 +1,16 @@
 <script setup>
 import Palette from "~icons/mdi/palette";
+
 const themePickerVisible = useThemePickerVisibility();
+const navbarVisible = useNavbarVisiblity();
+const router = useRouter();
+
+watch(themePickerVisible, () => (navbarVisible.value = false));
+router.afterEach(() => (themePickerVisible.value = false));
 </script>
 
 <template>
-  <button class="bg-words/10 rounded-full h-12 w-12 inline-flex items-center justify-center text-2xl" @click="themePickerVisible = !themePickerVisible">
+  <button class="bg-words/10 rounded-full h-12 w-12 inline-flex items-center justify-center text-2xl" :class="{ 'text-[#fff]': navbarVisible }" @click="themePickerVisible = !themePickerVisible">
     <span class="sr-only">open theme picker</span>
     <Palette />
   </button>
