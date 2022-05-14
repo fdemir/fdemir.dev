12787c55121a0179b92744909bc87a7b678ffcd0
Furkan Demir <frndemir@gmail.com>
2022-05-14 10:08:26 +0300

    :lipstick: add transition

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -7,11 +7,15 @@ const props = defineProps<{
 </script>
 
 <template>
-  <a :href="props.data.link" target="_blank" class="group cursor-pointer border-wordsoff/50 border-2 flex flex-col justify-between rounded-xl">
+  <a
+    :href="props.data.link"
+    target="_blank"
+    class="group cursor-pointer border-wordsoff/50 border-2 hover:border-primary flex flex-col justify-between rounded-xl transition-all duration-500 ease-in-out"
+  >
     <div class="p-6">
       <h3 class="font-semibold md:text-2xl text-xl py-4">{{ props.data.title }}</h3>
     </div>
-    <div class="py-3 px-6 border-wordsoff/50 border-t-2 w-full flex justify-between items-center">
+    <div class="py-3 px-6 border-wordsoff/50 border-t-2 group-hover:border-primary w-full flex justify-between items-center transition-all duration-500 ease-in-out">
       <span class="text-wordsoff">{{ props.data.pubDate }}</span>
     </div>
   </a>
--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -3,7 +3,7 @@ import Star from "~icons/mdi/star";
 </script>
 
 <template>
-  <a class="group cursor-pointer border-wordsoff/50 border-2 flex flex-col justify-between rounded-xl p-6">
+  <a class="group cursor-pointer border-wordsoff/50 border-2 flex flex-col justify-between rounded-xl p-6 hover:border-primary transiiton-all duration-500 ease-in-out">
     <div class="flex justify-between mb-6">
       <span class="text-2xl font-semibold">flex-logger</span>
       <div class="flex items-center">
@@ -11,7 +11,7 @@ import Star from "~icons/mdi/star";
         <Star class="text-2xl text-primary" />
       </div>
     </div>
-    <p class="h-10 overflow-hidden text-ellipsis block whitespace-nowrap">
+    <p class="lg:h-10 lg:overflow-hidden lg:text-ellipsis block lg:whitespace-nowrap">
       Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet
       consectetur.
     </p>
