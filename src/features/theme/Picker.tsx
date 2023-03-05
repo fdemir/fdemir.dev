92cf175ce018cbaea1439920381cea6e1a65c31c
Furkan Demir <48316033+fdemir@users.noreply.github.com>
2023-03-05 20:29:37 +0300

    Dev (#19)
    * feat: theme integration
    
    * feat: add transition to the body bg
    
    * feat: auto prefers color scheme
    
    * fix: remove persistent theme
    
    * feat: add color to date
    
    * fix: keep palette absolute
    
    * refactor: remove comments

---
+++ src/features/theme/Picker.tsx
@@ -0,0 +1,21 @@
+import "./Picker.css";
+import List from "./List";
+import rainbow from "./rainbow.svg?raw";
+import { createSignal, onMount } from "solid-js";
+
+function Picker() {
+  const [isOpen, setIsOpen] = createSignal(false);
+
+  return (
+    <>
+      <div className="rainbow-btn" onClick={() => setIsOpen(!isOpen())}>
+        <span innerHTML={rainbow}></span>
+      </div>
+
+      {/* TODO: load the list element when it is visible */}
+      {isOpen() && <List />}
+    </>
+  );
+}
+
+export default Picker;
