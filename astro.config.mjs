307a9c81da529769ecd709b5f730209f755f9da2
fdemir <frndemir@gmail.com>
2023-10-01 05:10:52 +0300

    feat: upgrade pkgs and manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
-import compress from "astro-compress";
-import robotsTxt from "astro-robots-txt";
-import image from "@astrojs/image";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fdemir.dev",
-  integrations: [robotsTxt({
-    sitemap: false
-  }), image(), compress(), solidJs()]
+  integrations: [solidJs()],
 });
